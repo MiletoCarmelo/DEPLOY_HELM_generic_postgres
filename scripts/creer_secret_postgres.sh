f9c95e42073a892d9539e2f7a40313632753f520
MiletoCarmelo <carmelo.mileto@gmail.com>
2024-11-21 08:36:34 +0100

    add

--- scripts/creer_secret_postgres.sh
+++ scripts/creer_secret_postgres.sh
@@ -3,15 +3,23 @@
 # Variables
 NAMESPACE="postgres"
 
-NAME_SCRET="pg-secrets"
+NAME_SCRET="postgres-secrets"
 
 # secrets from .env
 POSTGRES_DB=$(grep POSTGRES_DB .env | cut -d '=' -f2)
 POSTGRES_USER=$(grep POSTGRES_USER .env | cut -d '=' -f2)
 POSTGRES_PASSWORD=$(grep POSTGRES_PASSWORD .env | cut -d '=' -f2)
 
-# Créer le secret pour TS_AUTHKEY
-echo "🔒 Création du secret PG-SECRET .."
+# Vérifier si le namespace existe
+if ! kubectl get namespace ${NAMESPACE} >/dev/null 2>&1; then
+    echo "🔄 Création du namespace ${NAMESPACE}..."
+    kubectl create namespace ${NAMESPACE}
+else
+    echo "✅ Le namespace ${NAMESPACE} existe déjà"
+fi
+
+# Créer ou mettre à jour le secret
+echo "🔄 Création/mise à jour du secret ${NAME_SCRET} dans le namespace ${NAMESPACE}..."
 kubectl create secret generic ${NAME_SCRET} \
   --namespace=${NAMESPACE} \
   --from-literal=POSTGRES_DB=${POSTGRES_DB} \
